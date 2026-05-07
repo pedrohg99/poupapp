31ca66a6e08ebf715579d2e8e69683c940cb0618
Pedro Henrique <pedrohg99@yahoo.com>
2026-05-07 15:42:27 -0300

    Cria e estiliza componente de Tipografia

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Aside } from "./components/Aside";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
 import { SearchInput } from "./components/SearchInput";
+import { Typography } from "./components/Typography";
 
 function App() {
   return (
@@ -9,6 +10,14 @@ function App() {
       <Aside />
       <Main>
         <SearchInput />
+        <div>
+          <Typography variant="h1">
+            Olá, Pedro!
+          </Typography>
+          <Typography variant="body">
+            Veja como estão suas finanças hoje.
+          </Typography>
+        </div>
       </Main>
     </Container>
   );
