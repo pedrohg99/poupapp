3e135f249e2528e7a7adf0f60fa5b3d74d5949fd
Pedro Henrique <pedrohg99@yahoo.com>
2026-05-07 14:53:15 -0300

    Cria barra lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import logo from "./assets/logo.svg";
+import { Aside } from "./components/Aside";
 import { Container } from "./components/Container";
 
 function App() {
   return (
     <Container>
-      <aside>
-        <img src={logo} alt="" />
-      </aside>
+      <Aside />
     </Container>
   );
 }
