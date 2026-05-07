8316e72bf38c00c9f229bb46d7d81b89b63bf6d3
Pedro Henrique <pedrohg99@yahoo.com>
2026-05-07 15:01:32 -0300

    Cria componente SearchInput. Aplica no App.jsx. Cria pasta de icones.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Aside } from "./components/Aside";
 import { Container } from "./components/Container";
+import { SearchInput } from "./components/SearchInput";
 
 function App() {
   return (
     <Container>
       <Aside />
+      <main>
+        <SearchInput />
+      </main>
     </Container>
   );
 }
