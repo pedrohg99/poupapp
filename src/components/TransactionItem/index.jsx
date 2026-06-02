fb8bcff82406fe88ec7b8c8b965e1c5aa78fe4ab
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-02 16:51:19 -0300

    Configura card transacoes

--- src/components/TransactionItem/index.jsx
+++ src/components/TransactionItem/index.jsx
@@ -1,19 +1,18 @@
+import styles from './transactionitem.module.css'
+
+const formater = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "BRL" })
+
 export const TransactionItem = ({ item }) => {
 
     return (
-        <div>
-            <div>
-                <p>
-                    {item.description}
-                </p>
-                <p>
-                    {item.value}
-                </p>
+        <div className={styles.transaction}>
+            <div className={styles.details}>
+                <p>{item.description}</p>
+                <p>{formater.format(item.value)}</p>
             </div>
-            <div>
-                {item.date}
+            <div className={styles.date}>
+                {new Date(item.date).toLocaleDateString('pt-BR')}
             </div>
-
         </div>
     )
 }
--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../Button";
 import { IconCurrency } from "../Icons";
 import { TransactionItem } from "../TransactionItem";
+import styles from './transactions.module.css'
 
 export const Transactions = () => {
   const items = [
@@ -28,7 +29,7 @@ export const Transactions = () => {
 
   return (
     <>
-      <ul>
+      <ul className={styles.list}>
         {items.map((transactionItem, index) => {
           return (
             <li key={index}>
