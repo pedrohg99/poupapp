858e5c73b63c0ba42557e8023ef66b4038304a5a
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-09 10:46:37 -0300

    Aplica style para o botao

--- src/components/Transactions/index.jsx
+++ src/components/Transactions/index.jsx
@@ -39,9 +39,12 @@ export const Transactions = () => {
         })}
       </ul>
 
-      <Button>
-        <IconCurrency /> Adicionar transação
-      </Button>
+      <div className={styles.actions}>
+        <Button>
+          <IconCurrency /> Adicionar transação
+        </Button>
+      </div>
+
 
     </>
   );
