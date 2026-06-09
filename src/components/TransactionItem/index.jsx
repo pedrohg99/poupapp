f84b77308937cd961d2cd3a32cb4afcc3d026efd
Pedro Henrique <pedrohg99@yahoo.com>
2026-06-09 09:37:31 -0300

    Adiciona condicional de cor ao TransactionItem

--- src/components/TransactionItem/index.jsx
+++ src/components/TransactionItem/index.jsx
@@ -4,9 +4,11 @@ const formater = new Intl.NumberFormat("pt-BR", { style: "currency", currency: "
 
 export const TransactionItem = ({ item }) => {
 
+    const detailsAddicionalClassName = item.value >= 0 ? styles.income : styles.expense
+
     return (
         <div className={styles.transaction}>
-            <div className={styles.details}>
+            <div className={[styles.details, detailsAddicionalClassName].join(' ')}>
                 <p>{item.description}</p>
                 <p>{formater.format(item.value)}</p>
             </div>
